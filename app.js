3a4709db60426a8dd21df45a9ff52e8ee3b4d496
Niam <niam@Niams-MacBook-Pro.local>
2020-05-01 19:26:05 +0800

    Allow users to submit a secret

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String,
+  secret: String,
 });
 
 // SET THE PLUGIN FOR USER-SCHEMA AND USE THE PASSPORT-LOCAL-MONGOOSE
@@ -115,11 +116,39 @@ app.get('/register', function (req, res) {
 });
 
 app.get('/secrets', function (req, res) {
+  User.find({ secret: { $ne: null } }, function (err, foundUsers) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render('secrets', { usersWithSecrets: foundUsers });
+      }
+    }
+  });
+});
+
+app.get('/submit', function (req, res) {
   if (req.isAuthenticated()) {
-    res.render('secrets');
+    res.render('submit');
   } else {
     res.redirect('/login');
   }
+});
+
+app.post('/submit', function (req, res) {
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function () {
+          res.redirect('/secrets');
+        });
+      }
+    }
+  });
 });
 
 app.get('/logout', function (req, res) {
